0e962d380b6ad40ff3f846b22724206ce92a796a
Alen <info@flatcode.hr>
2022-08-19 01:08:24 +0200

    mobile navigation completed

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,9 +3,25 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log('data', data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payment infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequuntur beatae distinctio quae provident maxime! Perferendis
+            deleniti, quis dicta illo error doloribus.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
