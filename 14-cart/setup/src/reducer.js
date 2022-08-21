3899ce71c09dd3eab9188371eb8b4e4a119db434
Alen <info@flatcode.hr>
2022-08-21 14:33:01 +0200

    increase and decrease amount reducer actions completed

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -10,6 +10,30 @@ const reducer = (state, action) => {
     return { ...state, cart: filteredItems };
   }
 
+  if (action.type === 'INCREASE_ITEM') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === 'DECREASE_ITEM') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+
+    return { ...state, cart: tempCart };
+  }
+
   return state;
 };
 
