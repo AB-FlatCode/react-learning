cf9b01b380879a9fc5e3b8d6491b8242dfeb35aa
Alen <info@flatcode.hr>
2022-11-23 22:43:55 +0100

    navbar, static pages completed

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel libero, facere neque sequi debitis, quae veniam voluptatibus perspiciatis unde sunt quis commodi quas officiis
+        necessitatibus quod? Laborum,laboriosam assumenda in delectus, repudiandae ipsum fugit rem officiis deserunt praesentium quasi.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
