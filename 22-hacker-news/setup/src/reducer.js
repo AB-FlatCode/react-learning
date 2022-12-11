55d2a53725f1c571cd272246631be40361f77986
Alen <info@flatcode.hr>
2022-12-11 22:33:45 +0100

    initial data fetch reducer setup

--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -1,10 +1,15 @@
-import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
+import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } from './actions';
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+
+    case SET_STORIES:
+      return { ...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages };
+
+    default:
+      throw new Error(`No matchng type:"${action.type}" action type`);
+  }
+};
+export default reducer;
